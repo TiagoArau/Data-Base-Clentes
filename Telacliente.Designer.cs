f1f48f39f203de5e7c1a883271d5f941379d0bb7
Tiago Araujo <106354884+TiagoArau@users.noreply.github.com>
2022-10-26 17:51:13 -0300

    tela para o cliente

---
+++ Telacliente.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace CadastroCliente
+{
+    partial class Telacliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nome = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnsalvar = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.txtcpf = new System.Windows.Forms.MaskedTextBox();
+            this.txtdtn = new System.Windows.Forms.MaskedTextBox();
+            this.txtcelular = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nome.ForeColor = System.Drawing.SystemColors.Control;
+            this.Nome.Location = new System.Drawing.Point(170, 279);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(80, 22);
+            this.Nome.TabIndex = 0;
+            this.Nome.Text = "NOME:";
+            // 
+            // txtnome
+            // 
+            this.txtnome.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnome.Location = new System.Drawing.Point(256, 279);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(661, 30);
+            this.txtnome.TabIndex = 1;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(468, 376);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(449, 30);
+            this.txtemail.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(171, 381);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(532, 467);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 44);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Data de\r\nnascimento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(383, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "E-mail:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(227, 487);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Celular:";
+            // 
+            // btnsalvar
+            // 
+            this.btnsalvar.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalvar.Location = new System.Drawing.Point(346, 604);
+            this.btnsalvar.Name = "btnsalvar";
+            this.btnsalvar.Size = new System.Drawing.Size(106, 44);
+            this.btnsalvar.TabIndex = 7;
+            this.btnsalvar.Text = "Salvar";
+            this.btnsalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpar.Location = new System.Drawing.Point(628, 604);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(106, 44);
+            this.btnlimpar.TabIndex = 8;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            // 
+            // txtcpf
+            // 
+            this.txtcpf.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcpf.Location = new System.Drawing.Point(231, 376);
+            this.txtcpf.Mask = "000,000,000-00";
+            this.txtcpf.Name = "txtcpf";
+            this.txtcpf.Size = new System.Drawing.Size(146, 30);
+            this.txtcpf.TabIndex = 9;
+            // 
+            // txtdtn
+            // 
+            this.txtdtn.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdtn.Location = new System.Drawing.Point(662, 480);
+            this.txtdtn.Mask = "00/00/0000";
+            this.txtdtn.Name = "txtdtn";
+            this.txtdtn.Size = new System.Drawing.Size(109, 30);
+            this.txtdtn.TabIndex = 10;
+            this.txtdtn.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtcelular
+            // 
+            this.txtcelular.Font = new System.Drawing.Font("Clarendon BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcelular.Location = new System.Drawing.Point(316, 480);
+            this.txtcelular.Mask = "(00) 00000-0000";
+            this.txtcelular.Name = "txtcelular";
+            this.txtcelular.Size = new System.Drawing.Size(155, 30);
+            this.txtcelular.TabIndex = 11;
+            // 
+            // Telacliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1055, 848);
+            this.Controls.Add(this.txtcelular);
+            this.Controls.Add(this.txtdtn);
+            this.Controls.Add(this.txtcpf);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btnsalvar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.Nome);
+            this.Name = "Telacliente";
+            this.Text = "Telacliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnsalvar;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.MaskedTextBox txtcpf;
+        private System.Windows.Forms.MaskedTextBox txtdtn;
+        private System.Windows.Forms.MaskedTextBox txtcelular;
+    }
+}
